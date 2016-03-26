043404be7972e0cbba4bc3e048ee9eb65162b069
Omar Essam <omar_2412@live.com>
2016-03-26 22:24:12 +0200

    Initialized

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(request, resource){
+	resource.sendFile(__dirname + '/index.html');
+});
+
+app.use(express.static(__dirname + '/libs'));
+
+io.on('connection', function(socket){
+        console.log("New user!");
+
+	socket.on('disconnect', function(){
+		console.log("A loser left");
+	});
+
+	socket.on('chat-message', function(message){
+		console.log("Sent: " + message);
+		io.emit('chat-message', message);
+	});
+});
+
+http.listen(3000, function(){
+	console.log('listening on port 3000');
+});
+
