1852a34a3d4d465fdab6572e80bc86ad93d6afcd
Omar Essam <omar_2412@live.com>
2016-03-29 16:26:30 +0200

    Added username support

--- index.js
+++ index.js
@@ -2,27 +2,59 @@ var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var session;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/libs'));
+app.use(express.static(__dirname + '/public'));
+app.use(session({secret: 'SECRETKODE',
+				 saveUninitialized: true,
+                 resave: true}));
 
 app.get('/', function(request, resource){
+	session = request.session;
+	if(session.username) {
+		resource.redirect('/main');
+		return;
+	}
 	resource.sendFile(__dirname + '/index.html');
 });
 
 app.get('/main', function(request, resource){
+	session = request.session;
+	if(!session.username) {
+		resource.redirect('/');
+		return;
+	}
 	resource.sendFile(__dirname + '/main.html');
 });
 
-app.use(express.static(__dirname + '/libs'));
-app.use(express.static(__dirname + '/public'));
+app.post('/', function(request, resource){
+	username = request.body.username;
+	session = request.session;
+	session.username = username;
+	resource.redirect('/main');
+});
+
+app.get('/config', function(request, resource){
+	session = request.session;
+	resource.status(200).send({
+		username: session.username
+	});
+});
 
 io.on('connection', function(socket){
         console.log("New user!");
 
 	socket.on('disconnect', function(){
-		console.log("A loser left");
+		console.log("A user left");
 	});
 
 	socket.on('chat-message', function(message){
-		console.log("Sent: " + message);
+		console.log("Sent: " + message.message);
 		io.emit('chat-message', message);
 	});
 });
--- public/app.js
+++ public/app.js
@@ -1,22 +1,28 @@
 
 var app = angular.module('app', []);
-app.controller('groupCtrl', ['$scope', function ($scope) {
+app.controller('groupCtrl', ['$scope', '$http', function ($scope, $http) {
+    $scope.username = ""
+    grabUsername = function(){
+        $http.get('/config').success(function(data){
+            $scope.username = data.username;
+        });
+    };
+    grabUsername();
     var socket = io();
     $scope.message = "";
     $scope.texts = [];
     socket.on('chat-message', function(message){
         $scope.texts.push(message);
         $scope.$apply();
-    })
+    });
     $scope.chatMessage = function(){
-        socket.emit('chat-message', $scope.message);
+        socket.emit('chat-message', {
+            username: $scope.username,
+            message: $scope.message});
         $scope.message = '';
         return false;
     };
 }]);
 app.controller('usernameCtrl', ['$scope', function ($scope) {
     $scope.username = "";
-    $scope.Join = function(){
-        window.location.href = "/main";
-    };
 }]);
